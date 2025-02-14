72fe269958946b328fb4f9e87c0370a1a168204a
duanjc <duanjc@126.com>
2025-02-14 10:06:55 +0800

    用很蠢的方法添加双十一的运费计算。

---
+++ 接口代码与实战/DoubleElevenShippingCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 接口代码与实战
+{
+    internal class DoubleElevenShippingCalculator
+    {
+        public float CalculateShipping(Order order)
+        {
+            return 0;
+        }
+    }
+}
--- 接口代码与实战/OrderProcessor.cs
+++ 接口代码与实战/OrderProcessor.cs
@@ -9,9 +9,11 @@ namespace 接口代码与实战
      class OrderProcessor
     {
         private readonly ShippingCalculator  _shippingCalculator;
+        private readonly DoubleElevenShippingCalculator _doubleElevenShippingCalculator;
         public OrderProcessor()
         {
             _shippingCalculator = new ShippingCalculator();  //计算订单运费
+            _doubleElevenShippingCalculator = new DoubleElevenShippingCalculator(); 
         }
 
         public void Process(Order order)  
@@ -19,9 +21,15 @@ namespace 接口代码与实战
             if (order.IsShipped)   //判断订单是否已经被处理
                 throw new InvalidOperationException("订单已发货");
 
+            DateTime doubleEleven = new DateTime(2022, 11, 11);
+
             order.Shipment = new Shipment  //给订单创建一个发货信息 
-            {
-                Cost = _shippingCalculator.CalculateShipping(order),
+            {  
+                //判断日期是否是双十一，从而决定是否要免运费。
+                Cost =DateTime.Now==doubleEleven?
+                _doubleElevenShippingCalculator.CalculateShipping(order):
+                _shippingCalculator.CalculateShipping(order),
+
                 ShippingDate = DateTime.Today.AddDays(1)
             };
             
